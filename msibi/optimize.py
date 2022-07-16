c10dd7744ef52516468ff85fd213b0ff9b2bb183
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2022-07-16 15:32:25 -0600

    remove pair rswitch override in the _initialize func

--- msibi/optimize.py
+++ msibi/optimize.py
@@ -355,8 +355,6 @@ class MSIBI(object):
 
         for pair in self.pairs:
             if pair.pair_type == "table":
-                #TODO Fix handling of r_switch here?
-                pair.r_switch = pair.r_range[-5]
                 potential_file = os.path.join(
                     self.potentials_dir, f"pair_pot.{pair.name}.txt"
                 )
